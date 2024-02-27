ab436d187381a6996e9aa2b1252a7b8fde36b772
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2024-02-27 08:24:37 +0600

    feat: Added User Model & migration table

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table("users")->insert([
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'admin',
+                'status' => 'active',
+            ],
+            [
+                'name' => 'User',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'user',
+                'status' => 'active',
+            ],
+            [
+                'name' => 'Agent',
+                'email' => 'agent@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'agent',
+                'status' => 'active',
+            ]
+        ]);
+    }
+}
